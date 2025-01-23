22ca1547058cfcc8e2264e0f3c890dab4c6bbadb
FolksNP <Felipe70103@gmail.com>
2025-01-23 11:27:34 -0300

    metodo move criado, para modificar as posições dos games na lista.

--- src/main/java/com/politano/dslist/services/GameListService.java
+++ src/main/java/com/politano/dslist/services/GameListService.java
@@ -2,7 +2,9 @@ package com.politano.dslist.services;
 
 import com.politano.dslist.dto.GameListDTO;
 import com.politano.dslist.entities.GameList;
+import com.politano.dslist.projections.GameMinProjection;
 import com.politano.dslist.repositories.GameListRepository;
+import com.politano.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,12 +17,30 @@ public class GameListService {
     @Autowired
     private GameListRepository gameListRepository;
 
+    @Autowired
+    private GameRepository gameRepository;
+
     @Transactional(readOnly = true)
     public List<GameListDTO> findAll(){
         // framework faz a req no bd sozinho, e já tras como objeto
         List<GameList> result = gameListRepository.findAll();
         List<GameListDTO> dto = result.stream().map(x -> new GameListDTO(x)).toList();
         return dto;
+    }
+
+    @Transactional
+    public void move(Long listId, int sourceIndex, int destinationIndex){
+        List<GameMinProjection> list = gameRepository.searchByList(listId);
+        GameMinProjection obj = list.remove(sourceIndex);
+        list.add(destinationIndex, obj);
+
+        int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+        int max = sourceIndex > destinationIndex ? destinationIndex : sourceIndex;
+
+        for (int i = min; i <= max; i++){
+            gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+        }
+
     }
 
 }
